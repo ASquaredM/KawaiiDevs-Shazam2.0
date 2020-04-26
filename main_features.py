57efe123026dc17d72a7fa627e87d0ce073ebac6
OIMAS <iomar9606@gmail.com>
2020-04-26 15:18:56 +0200

    features_spectro

---
+++ main_features.py
@@ -0,0 +1,23 @@
+import librosa
+from scipy import signal
+
+filename = "01.mp3"
+
+def load_sound(filename):
+    
+    data, sr = librosa.load(filename,duration=60.0)
+    return [data,sr]
+
+def spectro(song,sr):
+    f, t, sxx = signal.spectrogram(x, sr)
+    return sxx
+
+
+def feature_1(song,sr):
+    centroid = librosa.feature.spectral_centroid(y=song, sr=sr)
+    return centroid
+
+def feature_2(song,sr):
+    spect_bw = librosa.feature.spectral_bandwidth(y=song, sr=sr)
+    return spect_bw
+
