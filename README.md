7da6c353acf8c635b89ac760a585281ab2c2ebb4
ProNoob <ProNoobWarning@github.com>
2020-05-19 16:45:35 +0200

    Minor Edits

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
 # KawaiiDevs-Shazam2.0
-A simple sound sampling and identification...  
-  
-More info about our contrubutions on:  
-https://github.com/ASquaredM/KawaiiDevs-Shazam2.0/tree/master  
-  
-Names:  
-Ahmed Alnouby youssef BN: 3  
-Ahmad Adel Muhammad BN: 7  
-Omar Ibrahim BN: 10
+
+A basic implementation of a Audio Recognition algorithm, it utilizes the advantages of a spectrogram and perceptual hashing, implementation is as follows :
+
+- A database is formed of 75 songs (Audio File) separated to their Vocal and Musical features.
+- Extraction of Spectrogram and spectral Features (Mel Spectrogram, Mel frequency Coefficient and Chroma STFT) is executed.
+- Hashing the extracted data with a Perceptual Hashing Algorithm.
+- A test Song (Audio File) is given to the application with extraction of its Hash the matches are found.
+- Matching percentages are calculated according to a mapping algorithm and then sorted to the user
+- A testing mechanism is implemented by mixing two Audio files then this mix is given to the application to find it's matches in the database
+
+## Basic Usage 
+
+This is a simple usage application only for testing the Recognition algorithm :
+
+- Load desired Audio File with max 2 files
+
+![Initial Screen](res/screenshot_1.png "Initial Screen")
+
+- Select the desired slider value from 0 to 100%
+
+- Click on the "Find Track" button
+
+![Search Results](res/screenshot_2.png "Search Results")
--- main.py
+++ main.py
@@ -7,7 +7,9 @@ import sounddevice as sd
 from SideWindowClass import SideWindowClass
 import UI.UI as UI
 from soundModel import SoundData as SD
-import database.GetSemelarity as GS 
+import database.GetSemelarity as GS
+import database.main_features as mf
+import imagehash
 
 
 class ApplicationWindow(UI.Ui_MainWindow):
@@ -16,7 +18,6 @@ class ApplicationWindow(UI.Ui_MainWindow):
 
         self.DisableMixer()
         self.Init()
-
         #print("Sorting Status", self.Similarity_View.isSortingEnabled())
 
         self.UpBtn_1.clicked.connect(lambda: self.GetTrack(0))
@@ -26,6 +27,7 @@ class ApplicationWindow(UI.Ui_MainWindow):
         self.MixerSlider.valueChanged.connect(self.mixTracks)
 
         self.SearchBtn.clicked.connect(self.Search)
+        
 
     def Init(self):
         self.Btns = [self.UpBtn_1, self.UpBtn_2]
@@ -60,20 +62,20 @@ class ApplicationWindow(UI.Ui_MainWindow):
             self.Snd[i].playAudio()
             self.SndUp[i] = True
             self.Btns[i].setChecked(True)
-            self.InsertAtIndex(i, 0, self.Snd[i].sndPath)
+            #self.InsertAtIndex(i, 0, self.Snd[i].sndPath)
             if self.SndUp[0] == self.SndUp[1] == True:
                 self.mixTracks()
-            
-            SongSimilarName=GS.returnInfo(GS.GetSimilerTo(self.Snd[i].sndByte,44100)[0])
 
-            self.InsertAtIndex(i, 1,SongSimilarName[0])
-            
+            SongSimilarName = GS.returnInfo(
+                GS.GetSimilerTo(self.Snd[i].sndByte, 44100)[0])
+
+            self.InsertAtIndex(i, 1, SongSimilarName[0])
 
     def mixTracks(self):
         self.EnableMixer()
         self.Mix = SD.mix(self.Snd[0], self.Snd[1],
                           self.MixerSlider.value() / 100.0)
-        self.Mix.playAudio()
+        #self.Mix.playAudio()
 
     def InsertAtIndex(self, y, x, Item):
         self.Similarity_View.setItem(y, x, QTableWidgetItem(Item))
@@ -83,10 +85,31 @@ class ApplicationWindow(UI.Ui_MainWindow):
         self.Similarity_View.setItem(y, x, QTableWidgetItem(""))
 
     def Search(self):
+        Song_Names = []
+        Song_SimIdx = []
+        index1, index2, Similarity = GS.GetSimilerTo(self.Mix.sndByte, 44100)
+        index = [index1, index2]
+        firstSongName, hash1Sim, firstfeature1Hash, firstfeature2Hash = GS.returnInfo(
+            index[0])
+        secondSongName, hash2Sim, secondfeature1Hash, secondfeature2Hash = GS.returnInfo(
+            index[1])
+        hash1Sim = Similarity[index[0]]
+        hash2Sim = Similarity[index[1]]
 
+        Song_Names.append(firstSongName)
+        Song_Names.append(secondSongName)
+        Song_SimIdx.append((100 - hash1Sim).__str__() + "%")
+        Song_SimIdx.append((100 - hash2Sim).__str__() + "%")
+        
+        for i in range(0, 9, 1):
+            if Song_Names[i] != None:
+                self.InsertAtIndex(i, 0, Song_Names[i])
+                self.InsertAtIndex(i, 1, Song_SimIdx[i])
+        """
         self.mainWindow=QtWidgets.QMainWindow()
-        self.sideWindow = SideWindowClass(self.mainWindow,self.Mix.sndByte,44100)
+        self.sideWindow = SideWindowClass(self.mainWindow,self.Mix.sndByte,)
         self.mainWindow.show()
+        """
 
 
 def main():
